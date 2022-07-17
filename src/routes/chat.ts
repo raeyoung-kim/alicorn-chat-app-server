a0168c4eae703f593e6b62dfa655c646bbcad605
raeyoung-kim <760kry@gmail.com>
2022-07-18 01:54:55 +0900

    socket 메시지 기능

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,22 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
-import Chat from '../models/Chat';
+import { getMessageList } from '../socket';
 
 const router = express.Router();
 
 router.get('/list', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.query;
-    const list = await (
-      await Chat.find({ chatId: new RegExp(`${userId}`, 'i') })
-    ).reverse();
 
-    const result = list.filter((item, i) => {
-      return (
-        list.findIndex((item2, j) => {
-          return item.chatId === item2.chatId;
-        }) === i
-      );
-    });
+    const result = await getMessageList(userId! as string);
 
     res.status(200).json(result);
   } catch (err) {
--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,26 @@
 import Chat from './models/Chat';
 
+export async function getMessageList(chatUser: string) {
+  const users = await (
+    await Chat.find({ chatId: new RegExp(`${chatUser}`, 'i') })
+  ).reverse();
+
+  const result = users.filter((item, i) => {
+    return (
+      users.findIndex((item2, j) => {
+        return item.chatId === item2.chatId;
+      }) === i
+    );
+  });
+
+  return result;
+}
+
 export default function socket(io: any) {
   io.on('connection', (socket: any) => {
     socket.on('join', async ({ id }: any) => {
       let chatId = id;
-      const newChatId = id.split('-').reverse().join('-');
+      const newChatId = id?.split('-').reverse().join('-');
       let chatList = await Chat.find({ chatId: chatId });
 
       if (!chatList?.length) {
@@ -15,17 +31,39 @@ export default function socket(io: any) {
       }
 
       if (chatList?.length) {
-        io.emit('message', { chatList, chatId });
+        socket.join(chatId);
+      } else {
+        socket.join(id);
       }
 
-      socket.join(chatId);
+      io.to(chatId).emit('message', { chatList, chatId });
+    });
+
+    socket.on('messageList', async ({ id }: any) => {
+      if (id) {
+        const result = await getMessageList(id);
+        socket.join(id);
+        io.to(id).emit('message_list', result);
+      }
     });
 
     socket.on('sendMessage', async (chatData: any, callback: () => void) => {
       await Chat.create(chatData);
       const result = await Chat.find({ chatId: chatData.chatId });
 
-      io.emit('message', result);
+      const users = chatData.chatId.split('-');
+
+      const result1 = await getMessageList(users[0]);
+      const result2 = await getMessageList(users[1]);
+
+      io.to(chatData.chatId).emit('message', {
+        chatList: result,
+        chatId: chatData.chatId,
+      });
+
+      io.to(`${users[0]}`).emit('message_list', result1);
+      io.to(`${users[1]}`).emit('message_list', result2);
+
       callback();
     });
 
